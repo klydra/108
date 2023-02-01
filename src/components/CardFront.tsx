d203e6a9a69329bdcc524814e7f4418719e41bef
FluVacc <justin10305@icloud.com>
2023-02-01 14:35:17 +0100

    Stunde 01.02.2023

--- src/components/CardFront.tsx
+++ src/components/CardFront.tsx
@@ -4,16 +4,16 @@ import {CardFace, CardType} from "../models/Card"
 export default function CardFront(props: { card: CardType }) {
   console.log(props.card)
 
-  return <div className="overflow-hidden h-52 aspect-[9/16] rounded p-1 bg-card-accent">
-    <div className="w-full h-[13%] rounded-t-lg bg-card-purple">
-      <p className="font-card text-[1.5rem] pr-1.5 pt-1 flex justify-end items-start w-full h-full text-background leading-none"><Face face={props.card.face} /></p>
+  return <div className="overflow-hidden h-52 aspect-[9/16] rounded-2xl p-1 bg-card-accent">
+    <div className="w-full h-[13%] rounded-t-xl bg-card-purple">
+      <p className="font-Card text-[1.5rem] pr-1.5 pt-1 flex justify-end items-start w-full h-full text-background leading-none"><Face face={props.card.face} /></p>
     </div>
     <div className="bg-card-purple flex justify-center items-center w-full h-[74%]">
       <div className="-rotate-card-angle rounded-card-inner flex justify-center items-center bg-card-accent flex justify-center items-center w-full h-[110%]">
         <p className="font-card rotate-card-angle text-[7rem] flex justify-center items-center w-full h-full text-background leading-none"><Face face={props.card.face} /></p>
       </div>
     </div>
-    <div className="w-full h-[13%] rounded-b-lg bg-card-purple">
+    <div className="w-full h-[13%] rounded-b-xl bg-card-purple">
       <p className="font-Card text-[1.5rem] pl-1.5 pb-1 flex justify-start items-end w-full h-full text-background leading-none"><Face face={props.card.face} /></p>
     </div>
   </div>
