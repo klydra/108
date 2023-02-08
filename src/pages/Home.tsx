3adc9d0f64242a11b5e1feb3710dc2a98f5a7344
FluVacc <justin10305@icloud.com>
2023-02-08 14:06:25 +0100

    Stunde 08.02.2023

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import {Component} from "react";
-import CardBack from "../components/CardBack";
-import {supabase} from "../lib/supabase";
+import React, {Component} from "react";
 import {Session, Subscription, User} from "@supabase/supabase-js";
+import CardFront from "../components/CardFront";
+import {CardColor, CardFace, CardType} from "../models/Card";
 
 interface HomeProps {
 
@@ -15,37 +14,15 @@ interface HomeState {
 }
 
 export default class Home extends Component<HomeProps, HomeState> {
-    constructor(props: HomeProps) {
-        super(props);
 
-        this.state = {
-            session: null,
-            auth: null,
-            user: null
-        }
-
+  render() {
+    const card: CardType = {
+      face: CardFace.NUMBER_0,
+      color: CardColor.GREEN
     }
 
-    componentDidMount() {
-        this.setState({ session: supabase.auth.session() })
-
-        const { data: auth } = supabase.auth.onAuthStateChange(
-          async (_, session) => {
-              this.setState({ user: session?.user ?? null })
-          }
-        )
-
-        this.setState({ auth })
-    }
-
-    componentWillUnmount() {
-        this.state.auth?.unsubscribe();
-    }
-
-
-    render() {
         return <div className="bg-background w-screen h-screen flex flex-column justify-center items-center">
-            <CardBack/>
+          <CardFront card={card}/>
         </div>;
     }
 }
