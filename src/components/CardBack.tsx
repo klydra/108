617c919bc04a5bfa8b989e605411314f50566c65
FluVacc <justin10305@icloud.com>
2023-02-13 14:52:02 +0100

    13.02.2023

--- src/components/CardBack.tsx
+++ src/components/CardBack.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function CardBack() {
-  return <div className="overflow-hidden h-52 aspect-[9/16] rounded p-1 bg-card-accent">
-    <div className="bg-card-back flex justify-center items-center w-full h-full">
-      <div className="-rotate-card-angle rounded-card-inner flex justify-center items-center bg-card-accent flex justify-center items-center w-full h-[80%]">
-        <p className="font-card rotate-card-back-angle text-[4rem] flex justify-center items-center w-full h-full text-card-back-text font-bold leading-none">108</p>
+  return <div className="overflow-hidden h-52 aspect-[9/16] rounded-2xl p-1 bg-card-accent">
+    <div className="bg-card-back flex justify-center items-center w-full h-[13%] rounded-t-[0.9rem]"></div>
+    <div className="bg-card-back flex justify-center items-center w-full h-[74%]">
+      <div className="-rotate-card-angle rounded-card-inner flex justify-center items-center p-[0.2rem] bg-card-accent flex justify-center items-center w-[85%] h-[115%]">
+        <div className="w-full h-full rounded-card-inner">
+          <div className="rotate-card-angle flex justify-center items-center w-full h-full">
+          <img src={"/icon.svg"} alt="" className="mr-0.5 -ml-0.5"/>
+          </div>
+        </div>
       </div>
     </div>
+    <div className="bg-card-back flex justify-center items-center w-full h-[13%] rounded-b-[0.9rem]"></div>
   </div>
 }
