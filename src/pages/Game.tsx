6cbea4d5788ff8d5f9e4b3565304d11a0e0d4da2
FluVacc <justin10305@icloud.com>
2023-02-14 15:08:14 +0100

    Animationen

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -8,7 +8,8 @@ export default class Game extends Component<any, any> {
     super(props);
 
     this.state = {
-      test: false
+      test: false,
+      cards: [1, 1, 1, 1]
     }
   }
 
@@ -26,55 +27,59 @@ export default class Game extends Component<any, any> {
         </div>
 
         {/* Own card row */}
-      <div className="fixed h-52 w-[60%] inset-x-[20%] bottom-[1%] flex gap-2 justify-center items-end">
-        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out aria-disabled:-translate-y-60 aria-disabled:scale-0 aria-disabled:duration-300 aria-disabled:opacity-0 " aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
-          <CardFront card={card} />
+        <div className="fixed h-52 w-[60%] inset-x-[20%] bottom-[1%] flex gap-2 justify-center items-end">
+          {this.state.cards.map(() => {
+            return <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out aria-disabled:-translate-y-60 aria-disabled:scale-0 aria-disabled:duration-300 aria-disabled:opacity-0 " aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
+              <CardFront card={card} />
+            </div>
+          })}
         </div>
-        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out aria-disabled:">
-          <CardFront card={card} />
-        </div>
-        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out">
-          <CardFront card={card} />
-        </div>
-        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out" aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
-          <CardFront card={card} />
-        </div>
-        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out">
-          <CardFront card={card} />
-        </div>
-      </div>
 
         {/* Left card row */}
-      <div className="fixed w-44 h-[80%] inset-y-[10%] left-[1%] rotate-180 flex flex-col justify-center items-end">
-          <CardBack rotated />
+        <div className="fixed w-44 h-[80%] inset-y-[10%] left-[1%] rotate-180 flex flex-col justify-center items-end">
+          <div className="w-full duration-100 ease-out aria-disabled:-translate-x-60 aria-disabled:scale-0 aria-disabled:duration-300 aria-disabled:opacity-0 " aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
+            <CardBack rotated />
+          </div>
         </div>
 
         {/* Right card row */}
         <div className="fixed w-44 h-[80%] inset-y-[10%] right-[1%] flex flex-col justify-center items-end">
-          <CardBack rotated/>
+          <div className="w-full duration-100 ease-out aria-disabled:-translate-x-60 aria-disabled:scale-0 aria-disabled:duration-300 aria-disabled:opacity-0" aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
+            <CardBack rotated/>
+          </div>
         </div>
 
         {/* Top card row */}
         <div className="fixed w-[80%] h-44 inset-x-[10%] top-[1%] gap-2 flex justify-center items-start">
-          <CardBack />
+          <div className="h-full duration-100 ease-out aria-disabled:translate-y-60 aria-disabled:scale-0 aria-disabled:duration-300 aria-disabled:opacity-0" aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
+            <CardBack />
+          </div>
           <CardBack />
           <CardBack />
         </div>
 
-        {/* Contest button */}
+        {/* Sort button */}
         <div className="fixed flex h-[12.5%] left-[10%] right-[80%] bottom-[6%]"></div>
 
         {/* Call button */}
         <div className="fixed flex h-[12.5%] left-[80%] right-[10%] bottom-[6%]"></div>
 
         {/* Draw stack */}
-        <div className="fixed flex inset-y-1/2 left-[37.5%] right-[50%] inset-y-[42%]">
+        <div className="fixed flex inset-y-1/2 left-[37.5%] right-[50%] inset-y-[42%] flex justify-center items-center">
+          <div className="h-full duration-700 ease-out aria-disabled:scale-[166%] aria-disabled:opacity-0 absolute" aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
+            <CardBack />
+          </div>
           <CardBack />
         </div>
 
         {/* Play stack  */}
-        <div className="fixed flex inset-y-1/2 right-[37.5%] left-[50%] inset-y-[42%]">
-          <CardFront card={card} />
+        <div className="fixed flex inset-y-1/2 right-[37.5%] left-[50%] inset-y-[42%] flex justify-center items-center" onClick={() => this.setState({ cards: this.state.cards.length === 4 ? [...this.state.cards, 1] : [1, 1, 1, 1] })}>
+          <div className="scale-75">
+            <CardFront card={card} />
+          </div>
+          <div className="scale-75 duration-700 ease-out aria-disabled:scale-125 aria-disabled:opacity-50 absolute " aria-disabled={this.state.test} onClick={() => this.setState({ cards: this.state.cards.length === 4 ? [...this.state.cards, 1] : [1, 1, 1, 1] })}>
+            <CardFront card={card} />
+          </div>
         </div>
       </>
     );
