542183dcfc44a494758e017d97defb348d03049c
FluVacc <justin10305@icloud.com>
2023-02-14 13:57:17 +0100

    Animationen

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./pages/Home";
+import Game from "./pages/Game";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
     <div>
-      <Home />
+      <Game />
     </div>
   </React.StrictMode>
 );
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -6,6 +6,10 @@ import CardBack from "../components/CardBack";
 export default class Game extends Component<any, any> {
   constructor(props: any) {
     super(props);
+
+    this.state = {
+      test: false
+    }
   }
 
   render() {
@@ -22,14 +26,23 @@ export default class Game extends Component<any, any> {
         </div>
 
         {/* Own card row */}
-        <div className="fixed h-52 w-[60%] inset-x-[20%] bottom-[1%] flex gap-2 justify-center items-end">
-          <CardFront card={card} />
-          <CardFront card={card} />
-          <CardFront card={card} />
-          <CardFront card={card} />
-          <CardFront card={card} />
+      <div className="fixed h-52 w-[60%] inset-x-[20%] bottom-[1%] flex gap-2 justify-center items-end">
+        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out aria-disabled:-translate-y-60 aria-disabled:scale-0 aria-disabled:duration-300 aria-disabled:opacity-0 " aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
           <CardFront card={card} />
         </div>
+        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out aria-disabled:">
+          <CardFront card={card} />
+        </div>
+        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out">
+          <CardFront card={card} />
+        </div>
+        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out" aria-disabled={this.state.test} onClick={() => this.setState({ test: !this.state.test })}>
+          <CardFront card={card} />
+        </div>
+        <div className="hover:-translate-y-3 hover:scale-110 duration-100 ease-out">
+          <CardFront card={card} />
+        </div>
+      </div>
 
         {/* Left card row */}
       <div className="fixed w-44 h-[80%] inset-y-[10%] left-[1%] rotate-180 flex flex-col justify-center items-end">
@@ -55,10 +68,14 @@ export default class Game extends Component<any, any> {
         <div className="fixed flex h-[12.5%] left-[80%] right-[10%] bottom-[6%]"></div>
 
         {/* Draw stack */}
-        <div className="fixed flex inset-y-1/2 left-[37.5%] right-[50%] inset-y-[42%]"></div>
+        <div className="fixed flex inset-y-1/2 left-[37.5%] right-[50%] inset-y-[42%]">
+          <CardBack />
+        </div>
 
         {/* Play stack  */}
-        <div className="fixed flex inset-y-1/2 right-[37.5%] left-[50%] inset-y-[42%]"></div>
+        <div className="fixed flex inset-y-1/2 right-[37.5%] left-[50%] inset-y-[42%]">
+          <CardFront card={card} />
+        </div>
       </>
     );
   }
